bebdf9ce73609224f4358321ff15e90ad426c703
njiti <kamaukan77@gmail.com>
2022-03-05 10:51:11 -0930

    updated button function

---
+++ app/src/main/java/com/example/mpishi/LandingActivity.java
@@ -0,0 +1,26 @@
+package com.example.mpishi;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.material.button.MaterialButton;
+
+public class LandingActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_landing);
+
+        MaterialButton loginbtn1 = (MaterialButton) findViewById(R.id.loginbtn1);
+
+        loginbtn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v){
+                startActivity(new Intent(LandingActivity.this,LandingActivity.class));
+            }
+        });
+    }
+}
