e911a34680b2d10fcd87ed7e2e84849d41342488
njiti <kamaukan77@gmail.com>
2022-03-06 22:50:52 -0930

    Downgraded my Main.java

--- app/src/main/java/com/example/mpishi/MainActivity.java
+++ app/src/main/java/com/example/mpishi/MainActivity.java
@@ -1,69 +1,13 @@
 package com.example.mpishi;
 
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.recyclerview.widget.GridLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
-
-import android.app.SearchManager;
-import android.content.Context;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuInflater;
-import android.view.MenuItem;
-import android.widget.ArrayAdapter;
-import android.widget.ListView;
-import android.widget.SearchView;
-import android.widget.Toast;
 
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
-
-import java.util.ArrayList;
-import java.util.Collections;
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
-
-    ListView listView;
-
-    public static final String API_KEY = "4ca0344ca45d4d38a11aa20db1ce7ce0";
-    public static final String BASE_URL = "https://api.spoonacular.com/recipes/716429/information?api_key=";
-
-    String name = BASE_URL+API_KEY;
-
-    ArrayAdapter<String> arrayAdapter;
-
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        listView =findViewById(R.id.listview);
-
-        arrayAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, Collections.singletonList(name));
-        listView.setAdapter(arrayAdapter);
-    }
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-
-        getMenuInflater().inflate(R.menu.menu,menu);
-
-        MenuItem menuItem = menu.findItem(R.id.action_search);
-        SearchView searchView = (SearchView) menuItem.getActionView();
-        searchView.setQueryHint("Type here to search");
-
-        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener(){
-            @Override
-            public boolean onQueryTextSubmit(String query){
-                return false;
-            }
-            @Override
-            public boolean onQueryTextChange(String newText){
-                arrayAdapter.getFilter().filter(newText);
-                return false;
-            }
-        });
-
-        return super.onCreateOptionsMenu(menu);
     }
 }
