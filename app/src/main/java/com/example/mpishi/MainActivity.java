cf436f5c3c3fb168b9077fc78017290d741d181b
njiti <kamaukan77@gmail.com>
2022-03-21 07:06:30 -0930

    Added a splash screen with lottie animations

--- app/src/main/java/com/example/mpishi/MainActivity.java
+++ app/src/main/java/com/example/mpishi/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.mpishi;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.window.SplashScreen;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -13,5 +16,14 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         FirebaseAuth.getInstance().signOut();
+
+        new Handler().postDelayed(new Runnable(){
+            @Override
+            public void run(){
+                Intent intent = new Intent(MainActivity.this,LandingActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        },25200);
     }
 }
